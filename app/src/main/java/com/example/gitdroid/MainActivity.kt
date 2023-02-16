d9dcf0adcb3e209035578f9819905031d8975a91
Alyona <borisovsk_a@mail.ru>
2023-02-16 13:13:25 +0300

    Initial

---
+++ app/src/main/java/com/example/gitdroid/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.gitdroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gitdroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+
+    }
+}
