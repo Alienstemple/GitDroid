64c13de9cb3de0dd14d7b95e387a2988bfd2de0b
Alyona <borisovsk_a@mail.ru>
2023-02-24 16:00:17 +0300

    feat: Navigation toggle

--- app/src/main/java/com/example/gitdroid/presentation/MainActivity.kt
+++ app/src/main/java/com/example/gitdroid/presentation/MainActivity.kt
@@ -3,22 +3,60 @@ package com.example.gitdroid.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.gitdroid.R
 import com.example.gitdroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-        startApp()
+        toggle = ActionBarDrawerToggle(this,
+            mainBinding.drawerLayout,
+            R.string.open_nav_drawer,
+            R.string.close_nav_drawer)
+        mainBinding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        mainBinding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item1 -> {
+                    Log.d(TAG, "Item 1 selected")
+                }
+                R.id.item2 -> {
+                    Log.d(TAG, "Item 2 selected")
+                }
+            }
+            true
+        }
+
+//        startApp()
 
     }
 
     private fun startApp() {
         intent = Intent(this, StartAppActivity::class.java)
         startActivity(intent)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        const val TAG = "MainActLog"
     }
 }
