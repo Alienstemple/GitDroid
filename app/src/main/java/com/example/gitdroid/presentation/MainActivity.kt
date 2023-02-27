0e9d03da7feaf708c913405b5a4c9f892971ffff
Alyona <borisovsk_a@mail.ru>
2023-02-27 09:39:44 +0300

    feat: Firebase auth first approach

--- app/src/main/java/com/example/gitdroid/presentation/MainActivity.kt
+++ app/src/main/java/com/example/gitdroid/presentation/MainActivity.kt
@@ -3,44 +3,84 @@ package com.example.gitdroid.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.gitdroid.R
 import com.example.gitdroid.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.OAuthProvider
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
     private lateinit var toggle: ActionBarDrawerToggle
+
+    private lateinit var firebaseUser: FirebaseUser
+    private lateinit var auth: FirebaseAuth
+    private val provider = OAuthProvider.newBuilder("github.com")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-        toggle = ActionBarDrawerToggle(this,
-            mainBinding.drawerLayout,
-            R.string.open_nav_drawer,
-            R.string.close_nav_drawer)
-        mainBinding.drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
+        with(mainBinding) {
+            // Action Drawer Menu
+            toggle = ActionBarDrawerToggle(this@MainActivity,
+                drawerLayout,
+                R.string.open_nav_drawer,
+                R.string.close_nav_drawer)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        mainBinding.navView.setNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.item1 -> {
-                    Log.d(TAG, "Item 1 selected")
+            navView.setNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.searchItem -> {
+                        Log.d(TAG, "Item 1 selected")
+                    }
+                    R.id.myProjItem -> {
+                        Log.d(TAG, "Item 2 selected")
+                    }
+                    R.id.repoAndIssItem -> {
+                        Log.d(TAG, "Item 3 selected")
+                    }
+                    R.id.settingsItem -> {
+                        Log.d(TAG, "Item 4 selected")
+                    }
                 }
-                R.id.item2 -> {
-                    Log.d(TAG, "Item 2 selected")
+                true
+            }
+
+            // Authorization
+            auth = FirebaseAuth.getInstance()
+
+            provider.addCustomParameter("login", enterEmailEditText.text.toString())
+
+            val scopes: ArrayList<String?> = object : ArrayList<String?>() {
+                init {
+                    add("user:email")
                 }
             }
-            true
+            provider.scopes = scopes
+
+            authBtn.setOnClickListener {
+                if (TextUtils.isEmpty(mainBinding.enterEmailEditText.text.toString())) {
+                    Toast.makeText(this@MainActivity, "Enter your github id", Toast.LENGTH_LONG).show()
+                } else {
+                    signInWithGithubProvider()
+                }
+            }
         }
 
-//        startApp()
+    }
+
+    private fun signInWithGithubProvider() {
 
     }
 
