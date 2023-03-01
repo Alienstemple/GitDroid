2ea2937ac3b0920e9525a237a91cb1cb478531ae
Alyona <borisovsk_a@mail.ru>
2023-03-01 11:43:16 +0300

    fix: Proj VM Factory

--- app/src/main/java/com/example/gitdroid/presentation/vm/ProjectsViewModelFactory.kt
+++ app/src/main/java/com/example/gitdroid/presentation/vm/ProjectsViewModelFactory.kt
@@ -2,7 +2,6 @@ package com.example.gitdroid.presentation.vm
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.gitdroid.domain.GithubInteractor
 import com.example.gitdroid.domain.ProjectsInteractor
 
 class ProjectsViewModelFactory(val projectsInteractor: ProjectsInteractor) :
