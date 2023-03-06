65685960108628dd376abef825d3d4325673ca4d
Alyona <borisovsk_a@mail.ru>
2023-03-06 16:58:23 +0300

    feat: Add search res to project working

--- app/src/main/java/com/example/gitdroid/presentation/fragments/CodeSearchFragment.kt
+++ app/src/main/java/com/example/gitdroid/presentation/fragments/CodeSearchFragment.kt
@@ -1,5 +1,6 @@
 package com.example.gitdroid.presentation.fragments
 
+import android.app.Dialog
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -11,24 +12,31 @@ import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gitdroid.R
 import com.example.gitdroid.data.GithubApiService
 import com.example.gitdroid.data.NetworkRepositoryImpl
 import com.example.gitdroid.data.NetworkService
 import com.example.gitdroid.data.SessionManager
 import com.example.gitdroid.databinding.FragmentCodeSearchBinding
+import com.example.gitdroid.databinding.ProjectsDialogBoxBinding
 import com.example.gitdroid.domain.GithubInteractorImpl
+import com.example.gitdroid.models.domain.Project
 import com.example.gitdroid.models.domain.SearchResultItem
 import com.example.gitdroid.presentation.MainActivity
+import com.example.gitdroid.presentation.adapters.ProjectsAdapter
 import com.example.gitdroid.presentation.adapters.SearchResultAdapter
+import com.example.gitdroid.presentation.misc.ProjectItemClickListener
 import com.example.gitdroid.presentation.misc.SearchResultItemClickListener
 import com.example.gitdroid.presentation.misc.navigation
 import com.example.gitdroid.presentation.vm.SearchResultViewModel
 import com.example.gitdroid.presentation.vm.GithubViewModelFactory
 import com.example.gitdroid.presentation.vm.ProjectsViewModel
 
-class CodeSearchFragment : Fragment(), SearchResultItemClickListener {
+class CodeSearchFragment : Fragment(), SearchResultItemClickListener, ProjectItemClickListener {
 
     private lateinit var binding: FragmentCodeSearchBinding
+    private lateinit var dialogBinding: ProjectsDialogBoxBinding
 
     private lateinit var searchResultViewModel: SearchResultViewModel
     private lateinit var searchResultAdapter: SearchResultAdapter
@@ -39,11 +47,14 @@ class CodeSearchFragment : Fragment(), SearchResultItemClickListener {
 
     private lateinit var projectSharedViewModel: ProjectsViewModel
 
+    private lateinit var selectedSearchResult: SearchResultItem
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentCodeSearchBinding.inflate(layoutInflater)
+        dialogBinding = ProjectsDialogBoxBinding.inflate(layoutInflater)
         return binding.root
     }
 
@@ -98,18 +109,40 @@ class CodeSearchFragment : Fragment(), SearchResultItemClickListener {
     }
 
     override fun onAddToProjectClicked(searchResultItem: SearchResultItem) {
-        Log.d(ProjectsFragment.TAG, "onAddToProjectClicked() called with: searchResultItem = $searchResultItem")
+        Log.d(ProjectsFragment.TAG,
+            "onAddToProjectClicked() called with: searchResultItem = $searchResultItem")
+
+        selectedSearchResult = searchResultItem  // Init selected search result
 
         projectSharedViewModel.testMethod()
         projectSharedViewModel.projectList
 
+        val dialogBox = Dialog(requireContext())
+        dialogBox.setContentView(dialogBinding.root)
 
-    // Get list of projects
-        // Alert dialog with recycler
-        // Item selected - call projectViewModel -- add
+        val projectsAdapter = ProjectsAdapter(this as ProjectItemClickListener)
+        dialogBinding.dialogRecyclerView.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        dialogBinding.dialogRecyclerView.adapter = projectsAdapter
+
+        projectSharedViewModel.projectList.observe(viewLifecycleOwner) { projectItemsList ->
+            projectItemsList?.let {
+                // Обновляем Recycler View
+                projectsAdapter.setList(it)
+            }
+        }
+
+        dialogBox.show()
+    }
+
+    override fun onItemClicked(project: Project) {
+        Log.d(TAG, "onItemClicked() called with: project = $project")
+        projectSharedViewModel.updateProject(project.id, selectedSearchResult)
     }
 
     companion object {
+        const val TAG = "CodeSearchFragLog"
+
         @JvmStatic
         fun newInstance() =
             CodeSearchFragment()
