e6414b3d2e0df0cca4335b7ac0bf2d3426942e19
Alyona <borisovsk_a@mail.ru>
2023-03-06 10:48:32 +0300

    fix: Fix for build

--- app/src/main/java/com/example/gitdroid/models/domain/Project.kt
+++ app/src/main/java/com/example/gitdroid/models/domain/Project.kt
@@ -3,6 +3,7 @@ package com.example.gitdroid.models.domain
 import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "projects")
@@ -15,5 +16,6 @@ data class Project(
     @ColumnInfo(name = "name")
     var name: String = "",
 //    val searchResList: List<String> = emptyList()  // TODO search results
+    @Ignore
     var searchResList: List<SearchResultItem> = emptyList()  // TODO как хранить search res в бд?
 )
